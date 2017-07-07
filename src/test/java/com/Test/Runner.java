46e305875e379624cc4660491b90118df43c4317
veera <prathap.ufttest@gmail.com>
2017-07-08 00:31:24 +0530

    doing first commit

---
+++ src/test/java/com/Test/Runner.java
@@ -0,0 +1,25 @@
+package com.Test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class Runner {
+	
+	@Test
+	
+	public static void LaunchBrowser()
+	{
+		WebDriver driver=new FirefoxDriver();
+		driver.navigate().to("www.irctc.co.in");
+		
+		driver.manage().window().maximize();
+		
+		
+	}
+	
+	
+	
+	
+
+}
